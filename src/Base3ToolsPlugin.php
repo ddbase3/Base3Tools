305de5a28e0bad08f392702192fc0c15f56ee6ec
Daniel Dahme <dahme@qualitus.de>
2025-03-25 19:44:20 +0100

    plugin creation

---
+++ src/Base3ToolsPlugin.php
@@ -0,0 +1,34 @@
+<?php declare(strict_types=1);
+
+namespace Base3Tools;
+
+use Api\IPlugin;
+use Base3\ServiceLocator;
+
+class Base3ToolsPlugin implements IPlugin {
+
+	private $servicelocator;
+
+	public function __construct() {
+		$this->servicelocator = ServiceLocator::getInstance();
+	}
+
+	// Implementation of IBase
+
+	public function getName() {
+		return "base3toolsplugin";
+	}
+
+	// Implementation of IPlugin
+
+	public function init() {
+
+		$this->servicelocator
+
+			->set(
+				$this->getName(),
+				$this,
+				ServiceLocator::SHARED);
+	}
+
+}
